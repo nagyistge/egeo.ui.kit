34385a2902ff76b4e6dc1f4f5910c110ce6614cc
alexrchies <alexrchies@gmail.com>
2015-10-14 17:59:12 +0200

    feature(template): add the angular library

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,7 +112,7 @@ module.exports = function (grunt) {
       doc: {
         options: {
           cmd: function(f) {
-            return '.\\node_modules\\.bin\\kss-node --source src --source node_modules/egeo.ui.base/dist/sass --destination dist --template node_modules/egeo.website.template/dist/kss-template --homepage readme.md --css public/styleguide.css';
+            return '.\\node_modules\\.bin\\kss-node --source src --source node_modules/egeo.ui.base/dist/sass --destination dist --template node_modules/egeo.website.template/dist/kss-template --homepage readme.md --css public/styleguide.css --js public/js/angular/angular.min.js';
           }
         },
         files: [{
