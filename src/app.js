64a7ec6d448f661c65a5205b047d5a69ebc6cef8
alexrchies <alexrchies@gmail.com>
2015-10-19 13:17:04 +0200

    refactor(grunt): change the sass src folder

---
+++ src/app.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name webApp
+     * @description
+     * # webApp
+     *
+     * Main module of the application.
+     */
+
+    angular
+        .module('myApp', [
+            'egeo.buttons'
+        ]);
+
+//        .config(function(EgeoConfigProvider){
+  //          EgeoConfigProvider.setEgeoPath('public/js/egeo');
+    //    });
+})();
