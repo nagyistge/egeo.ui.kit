0f60cf7ac53a8b6c58b54d4572cf0e8f10c84a3b
alexrchies <alexrchies@gmail.com>
2015-10-19 15:14:31 +0200

    feature(config): add config options to set the Egeo folder

--- generate-doc.bat
+++ generate-doc.bat
@@ -1,4 +1,4 @@
 rm -R dist
-.\node_modules\.bin\kss-node --source src --source node_modules/egeo.ui.base/dist/egeo --destination dist --template node_modules/egeo.website.template/dist/kss-template --homepage readme.md --css public/styleguide.css --js public/js/angular/angular.min.js;
+.\node_modules\.bin\kss-node --source src --source node_modules/egeo.ui.base/dist/egeo --destination dist --template node_modules/egeo.website.template/dist/kss-template --homepage readme.md --css public/styleguide.css --js public/js/angular/angular.min.js --js public/js/app.js --js public/js/egeo/components/button/components.button.directive.js --js public/js/egeo/providers/egeo.config.provider.js;
 pause
  
